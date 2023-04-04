7a8023b8c8293e3b497b66a416a66eab999e4fc4
yinotherhino <muhammedbayero@gmail.com>
2023-04-04 20:00:58 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
-import Image from 'next/image'
-import {  } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Lato } from "next/font/google";
+import styles from "./page.module.css";
+import {GrHomeRounded} from "react-icons/gr";
+import {SiEsphome} from "react-icons/si";
+import {BsJournalBookmark} from "react-icons/bs";
 
-const inter = Inter({ subsets: ['latin'] })
+const lato = Lato({
+  weight: ["300", "400", "700", "900"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <div>
-
+    <div className={lato.className + " parent"}>
+      <div className={styles["left-column"]}>
+        <h1>Flymoney</h1>
+        <div>
+          <h3>MAIN MENU</h3>
+          <ul>
+            <li><GrHomeRounded /> Home</li>
+            <li><SiEsphome /> Bank accounts</li>
+            <li><BsJournalBookmark/> Account statement</li>
+          </ul>
+        </div>
+        <div>
+          <h3>PREFERENCES</h3>
+          <ul>
+            <li>Settings</li>
+            <li>Support</li>
+            <li>Logout</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
+      colors: {
+        white: "#f5f5f5",
+        secondaryColor: "#5db1a2",
+        overlaycolor: "#1c0140",
+        lightPurple: "#e8e6ec",
+        tertiaryColor: "#3b0284",
+        primaryColor: "#dfdfdf",
+      },
+      screens: {
+        xxxs: "280px",
+        xxs: "320px",
+        xs: "480px",
+        xsm: "600px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1280px",
+        xl: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
